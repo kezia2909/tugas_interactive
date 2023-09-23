43a916fe2f05bb95056e43211003360473a5a0c1
kezia2909 <c14190048@john.petra.ac.id>
2023-09-23 08:16:40 +0700

    infinite scroll oke, both side oke

--- lib/pages/gallery_page.dart
+++ lib/pages/gallery_page.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class GalleryPage extends StatefulWidget {
@@ -8,8 +9,68 @@ class GalleryPage extends StatefulWidget {
 }
 
 class _GalleryPageState extends State<GalleryPage> {
+  List<String> listOfItemsLeft = ["1", "2", "3", "4", "5"];
+  List<String> listOfItemsRight = ["A", "B", "C", "D", "E"];
+  List<String> listOfItemsBoth = ["A1", "B2", "C3", "D4", "E5"];
   @override
   Widget build(BuildContext context) {
-    return Text("gallery");
+    return Column(
+      children: [
+        // left only
+        CarouselSlider(
+          options: CarouselOptions(height: 200),
+          items: listOfItemsLeft.map((i) {
+            return Builder(
+              builder: (BuildContext context) {
+                return Container(
+                    width: MediaQuery.of(context).size.width,
+                    margin: EdgeInsets.symmetric(horizontal: 5.0),
+                    decoration: BoxDecoration(color: Colors.amber),
+                    child: Text(
+                      'text $i',
+                      style: TextStyle(fontSize: 16.0),
+                    ));
+              },
+            );
+          }).toList(),
+        ),
+        // right only
+        CarouselSlider(
+          options: CarouselOptions(height: 200),
+          items: listOfItemsRight.map((i) {
+            return Builder(
+              builder: (BuildContext context) {
+                return Container(
+                    width: MediaQuery.of(context).size.width,
+                    margin: EdgeInsets.symmetric(horizontal: 5.0),
+                    decoration: BoxDecoration(color: Colors.amber),
+                    child: Text(
+                      'text $i',
+                      style: TextStyle(fontSize: 16.0),
+                    ));
+              },
+            );
+          }).toList(),
+        ),
+        // both side
+        CarouselSlider(
+          options: CarouselOptions(height: 200),
+          items: listOfItemsBoth.map((i) {
+            return Builder(
+              builder: (BuildContext context) {
+                return Container(
+                    width: MediaQuery.of(context).size.width,
+                    margin: EdgeInsets.symmetric(horizontal: 5.0),
+                    decoration: BoxDecoration(color: Colors.amber),
+                    child: Text(
+                      'text $i',
+                      style: TextStyle(fontSize: 16.0),
+                    ));
+              },
+            );
+          }).toList(),
+        ),
+      ],
+    );
   }
 }
