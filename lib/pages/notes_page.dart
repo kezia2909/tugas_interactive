9e66b101da2dbbc0c49572d8a76338bf3a1fc698
kezia2909 <c14190048@john.petra.ac.id>
2023-09-23 11:46:07 +0700

    set input validation

--- lib/pages/notes_page.dart
+++ lib/pages/notes_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_application_1/helper/sql_helper.dart';
 import 'package:flutter_application_1/models/notes_model.dart';
 
@@ -19,6 +20,44 @@ class _NotesPageState extends State<NotesPage> {
   void initState() {
     super.initState();
     _loadNotes();
+    _controllerNilai.addListener(
+      () {
+        if (_controllerNilai.text == ".") {
+          setState(() {
+            _controllerNilai.text = "0.";
+          });
+        }
+        inputValidation();
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _controllerNilai.dispose();
+    super.dispose();
+  }
+
+  void inputValidation() {
+    String input = _controllerNilai.text.trim();
+    print("input : $input");
+    if (input.isNotEmpty) {
+      final tempNilai = double.tryParse(input);
+      if (tempNilai != null) {
+        print("oke : $tempNilai");
+        print("temp : $tempNilai");
+        if (tempNilai > 100) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text('Input max 100.0'),
+            ),
+          );
+        }
+      } else {
+        print("error : $input");
+      }
+    }
+    setState(() {});
   }
 
   Future<void> _loadNotes() async {
@@ -28,8 +67,25 @@ class _NotesPageState extends State<NotesPage> {
     });
   }
 
-  void addNotes(String nilai) async {
+  String convertGrade(double nilai) {
     String grade = "E";
+    if (nilai >= 85.5) {
+      grade = "A";
+    } else if (nilai >= 75.5) {
+      grade = "B+";
+    } else if (nilai >= 68.5) {
+      grade = "B";
+    } else if (nilai >= 60.5) {
+      grade = "C+";
+    } else if (nilai >= 55.5) {
+      grade = "C";
+    } else if (nilai >= 40.5) {
+      grade = "D";
+    }
+    return grade;
+  }
+
+  void addNotes(String nilai) async {
     double? tempNilai;
     try {
       tempNilai = double.tryParse(nilai);
@@ -38,26 +94,12 @@ class _NotesPageState extends State<NotesPage> {
       } else {
         print("oke : $tempNilai");
         print("temp : $tempNilai");
-        if (tempNilai >= 85.5) {
-          grade = "A";
-        } else if (tempNilai >= 75.5) {
-          grade = "B+";
-        } else if (tempNilai >= 68.5) {
-          grade = "B";
-        } else if (tempNilai >= 60.5) {
-          grade = "C+";
-        } else if (tempNilai >= 55.5) {
-          grade = "C";
-        } else if (tempNilai >= 40.5) {
-          grade = "D";
-        }
-        // setState(() {
-        //   listOfNotes.add(NotesModel(nilai: tempNilai!, grade: grade));
-        //   print("List : ${listOfNotes}");
-        // });
-        final newNote = NotesModel(nilai: tempNilai, grade: grade);
+
+        final newNote =
+            NotesModel(nilai: tempNilai, grade: convertGrade(tempNilai));
         print("NEW NOTE : $newNote");
         await sqlHelper.insertNotes(newNote);
+
         setState(() {
           _loadNotes();
         });
@@ -69,39 +111,54 @@ class _NotesPageState extends State<NotesPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TextField(
-          controller: _controllerNilai,
-          keyboardType: TextInputType.number,
-        ),
-        ElevatedButton(
-          child: Text("Submit"),
-          onPressed: () {
-            addNotes(_controllerNilai.text.toString());
-          },
-        ),
-        Expanded(
-          child: ListView.builder(
-            itemCount: listOfNotes.length,
-            itemBuilder: (context, index) {
-              return Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Nilai : ${listOfNotes[index].nilai}",
-                    style: TextStyle(fontSize: 30),
-                  ),
-                  Text(
-                    "Nilai : ${listOfNotes[index].grade}",
-                    style: TextStyle(fontSize: 30),
-                  ),
-                ],
-              );
+    return Container(
+      padding: EdgeInsets.fromLTRB(16, 24, 16, 0),
+      child: Column(
+        children: [
+          TextField(
+            controller: _controllerNilai,
+            keyboardType: TextInputType.numberWithOptions(decimal: true),
+            inputFormatters: [
+              FilteringTextInputFormatter.allow(RegExp(r'^\d*\.?\d{0,1}')),
+            ],
+            decoration: InputDecoration(
+              hintText: "ex : 95.6, 70, etc",
+              label: Text("nilai (0.0-100.0)"),
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                  borderSide:
+                      const BorderSide(width: 0, style: BorderStyle.solid)),
+            ),
+          ),
+          ElevatedButton(
+            child: Text("Submit"),
+            onPressed: () {
+              addNotes(_controllerNilai.text.toString());
+              _controllerNilai.clear();
             },
           ),
-        )
-      ],
+          Expanded(
+            child: ListView.builder(
+              itemCount: listOfNotes.length,
+              itemBuilder: (context, index) {
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Nilai : ${listOfNotes[index].nilai}",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      "Nilai : ${listOfNotes[index].grade}",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                  ],
+                );
+              },
+            ),
+          )
+        ],
+      ),
     );
   }
 }
